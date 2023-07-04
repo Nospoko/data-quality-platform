23272b46d7808ca27431a4c09edea44e1f841b5a
Tomek Roszczynialski <roszcz@users.noreply.github.com>
2023-07-04 06:49:17 +0200

    QRS-8: Database initialization (#2)
    * create postgres table with metadata of all available records
    
    * update readme
    
    * add index to the record table and cache datasets

--- data-api/app/main.py
+++ data-api/app/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 from datasets import load_dataset
 
+from app import utils as app_utils
+
 
 app = FastAPI()
 dataset = load_dataset("roszcz/qrs-swipe-demo", split="train")
+
+app_utils.prepare_database(dataset)
 
 
 @app.get("/ping")
