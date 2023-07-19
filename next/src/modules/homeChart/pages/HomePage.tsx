89f0dcf3bd609efc88ff67944ced84a9c94d6fd3
Vitalii <136322674+VitalInnovator@users.noreply.github.com>
2023-07-19 21:43:40 +0300

    Fix sign in (#9)

--- next/src/modules/homeChart/pages/HomePage.tsx
+++ next/src/modules/homeChart/pages/HomePage.tsx
@@ -73,10 +73,6 @@ const HomePage = () => {
     fetchNextPage();
   }, [recordsToDisplay.length, fetchNextPage]);
 
-  if (status === 'unauthenticated') {
-    return <h1>Please sign in</h1>;
-  }
-
   const handleOpenModal = useCallback((chartData: SelectedChartData) => {
     setSelectedChartData(chartData);
     setOpenModal(true);
@@ -90,6 +86,10 @@ const HomePage = () => {
   const handleFetchNextPage = useCallback(() => {
     fetchNextPage();
   }, [fetchNextPage]);
+
+  if (status === 'unauthenticated') {
+    return <h1>Please sign in</h1>;
+  }
 
   return (
     <>
