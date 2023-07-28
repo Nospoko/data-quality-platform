88181789e126247ee146a74b523aeac53487ffb4
Tomasz Roszczynialski <roszcz@gmail.com>
2023-07-28 20:41:56 +0200

    update api

--- data-api/app/main.py
+++ data-api/app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://pqrst.tech"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
