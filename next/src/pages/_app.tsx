09e751fda458ccc6b62852334d278b9eeeeab7c0
VitalInnovator <aguilavolandoenladistancia@gmail.com>
2023-08-18 17:41:04 +0300

    chore: add DATA_PROBLEM check to app

--- next/src/pages/_app.tsx
+++ next/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Typography } from 'antd';
 import { NextPage } from 'next';
 import { SessionProvider } from 'next-auth/react';
 import { ReactElement, ReactNode } from 'react';
@@ -19,14 +20,33 @@ type AppPropsWithLayout = AppProps & {
 
 const queryClient = new QueryClient();
 
+const ALLOWED_DATA_PROBLEMS = ['ecg_classification', 'midi_review'];
+const verifyDataProblem = (dataProblem: string | undefined) => {
+  if (!dataProblem) {
+    return false;
+  }
+  return ALLOWED_DATA_PROBLEMS.includes(dataProblem);
+};
+
 const App = ({ Component, pageProps }: AppPropsWithLayout) => {
   const getLayout = Component.getNestedLayout ?? ((page) => page);
+
   return (
     <SessionProvider session={pageProps.session}>
       <QueryClientProvider client={queryClient}>
         <GlobalStyle />
         <ThemeProvider clearTransition>
-          {getLayout(<Component {...pageProps} />)}
+          {getLayout(
+            verifyDataProblem(process.env.NEXT_PUBLIC_DATA_PROBLEM) ? (
+              <Component {...pageProps} />
+            ) : (
+              <Typography.Title type="danger">
+                The value provided for DATA_PROBLEM is not recognized.
+                Application functionality is unavailable with the current
+                setting
+              </Typography.Title>
+            ),
+          )}
         </ThemeProvider>
       </QueryClientProvider>
     </SessionProvider>
