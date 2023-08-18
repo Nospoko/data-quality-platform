7d8d74360fd3cbc29cbe1e57c46e3c6701473770
VitalInnovator <aguilavolandoenladistancia@gmail.com>
2023-08-18 08:10:56 +0300

    chore: add DATA_PROBLEM check

---
+++ next/start-prod.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ "$DATA_PROBLEM" != "ecg_classification" ]; then
+  echo "DATA_PROBLEM must be set to 'ecg_classification' to start the app. Current value: $DATA_PROBLEM"
+  exit 1
+fi
+
+yarn migrate && yarn start
