4b2fd1943351cedac47e64f0b108c2dc20f45d49
VitalInnovator <aguilavolandoenladistancia@gmail.com>
2023-08-31 16:35:57 +0200

    remove hardcoded dataset

--- next/src/pages/api/records/list.ts
+++ next/src/pages/api/records/list.ts
@@ -15,8 +15,7 @@ router.get(async (req, res) => {
   if (!session) {
     return res.status(401).json({ error: 'Unauthorized User' });
   }
-  // const datasetName = req.query.datasetName as string;
-  const datasetName = 'roszcz/qrs-swipe-demo';
+  const datasetName = req.query.datasetName as string;
   const limit = Number(req.query.limit) || 10;
   const exams = req.query['exams[]'];
   const examIds = Array.isArray(exams) ? exams : [exams];
